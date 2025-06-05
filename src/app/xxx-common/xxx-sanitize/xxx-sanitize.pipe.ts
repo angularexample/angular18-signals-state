0e6c19292c759575d0420c92a78527801019273c
angularexample <info@angularexample.com>
2025-06-05 11:04:04 -0400

    Update SCSS to use partials, replace @import with @use. Add xxx-sanitize.pipe.ts. Home content is html.

---
+++ src/app/xxx-common/xxx-sanitize/xxx-sanitize.pipe.ts
@@ -0,0 +1,19 @@
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  standalone: true,
+  name: "xxxSanitize"
+})
+export class XxxSanitizePipe implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  transform(value: unknown): SafeHtml {
+    if (value) {
+      return this.sanitizer.bypassSecurityTrustHtml(<string>value);
+    } else {
+      return '';
+    }
+  }
+}
--- src/app/xxx-home/xxx-home.component.ts
+++ src/app/xxx-home/xxx-home.component.ts
@@ -2,11 +2,13 @@ import {ChangeDetectionStrategy, Component, inject, Signal} from '@angular/core'
 import {XxxContent} from "../xxx-common/xxx-content/xxx-content.types";
 import {XxxContentComponent} from '../xxx-common/xxx-content/xxx-content.component';
 import {XxxContentFacade} from "../xxx-common/xxx-content/xxx-content-facade.service";
+import {XxxSanitizePipe} from '../xxx-common/xxx-sanitize/xxx-sanitize.pipe';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     XxxContentComponent,
+    XxxSanitizePipe
   ],
   selector: 'xxx-home',
   standalone: true,
