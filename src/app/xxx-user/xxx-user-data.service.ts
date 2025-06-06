4d3bffde3c6da16cf62d1b989250e65c247bc6c1
angularexample <info@angularexample.com>
2025-06-06 15:34:33 -0400

    Remove spec files. Add delay to user service.

--- src/app/xxx-user/xxx-user-data.service.ts
+++ src/app/xxx-user/xxx-user-data.service.ts
@@ -4,14 +4,14 @@ import {Observable} from "rxjs";
 import {XxxUserApiResponse} from "./xxx-user.types";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class XxxUserDataService {
-    private http: HttpClient = inject(HttpClient);
+  private http: HttpClient = inject(HttpClient);
 
-    getUsers(): Observable<XxxUserApiResponse> {
-        // const url = 'https://dummyjson.com/users/?delay=3000'
-        const url = 'https://dummyjson.com/users';
-        return this.http.get<XxxUserApiResponse>(url);
-    }
+  getUsers(): Observable<XxxUserApiResponse> {
+    // delay added to see loading component
+    const url = 'https://dummyjson.com/users/?delay=2000'
+    return this.http.get<XxxUserApiResponse>(url);
+  }
 }
